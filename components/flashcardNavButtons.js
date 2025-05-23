414a79f80cab6788f654c7bd4aa611a430b7149e
David Hurrell <dhurrell@hotmail.co.uk>
2025-05-23 17:43:51 +0100

    feat: Add FlashcardNavButtons component for navigation in flashcard interface

---
+++ components/flashcardNavButtons.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import SmallSelectorButton from "./smallSelectorButton";
+import AntDesign from "@expo/vector-icons/AntDesign";
+export default function FlashcardNavButtons({
+  labelArray,
+  handleNext,
+  handlePrev,
+  handleFlip,
+  isCardReversed,
+}) {
+  return (
+    <View style={styles.buttonContainer}>
+      <SmallSelectorButton
+        text={labelArray[0]}
+        icon={<AntDesign name="banckward" size={20} color="black" />}
+        onPress={handlePrev}
+      />
+      <SmallSelectorButton text={labelArray[1]} onPress={handleFlip} />
+      <SmallSelectorButton
+        text={labelArray[2]}
+        icon={<AntDesign name="forward" size={20} color="black" />}
+        onPress={handleNext}
+      />
+    </View>
+  );
+}
+styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "100%",
+    marginTop: 20,
+  },
+});

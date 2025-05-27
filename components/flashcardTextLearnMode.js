370c8a6be3d8aefc74e774f6b7fe6fbe0216f66e
David Hurrell <dhurrell@hotmail.co.uk>
2025-05-27 18:56:49 +0100

    refactor: Remove unused styles and adjust card text size for improved layout

--- components/flashcardTextLearnMode.js
+++ components/flashcardTextLearnMode.js
@@ -26,47 +26,13 @@ export default function FlashcardTextLearnMode({
   );
 }
 const styles = StyleSheet.create({
-  flashcard: {
-    width: "85%",
-    minHeight: 200,
-    margin: 20,
-    backgroundColor: "#fff",
-    borderRadius: 5,
-    // iOS shadow
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-
-    // Android shadow
-    elevation: 5,
-  },
-
-  flashcardFront: {
-    width: "100%",
-    minHeight: 200,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth: 2,
-    borderColor: "gold",
-    padding: 5,
-    fontSize: 25,
-    // backgroundColor: "blue",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    width: "100%",
-    marginTop: 20,
-  },
   cardText: {
-    fontSize: 20,
+    fontSize: 17,
     textAlign: "center",
     marginBottom: 20,
   },
   scrollContainer: {
     width: "100%",
-    height: 400,
+    height: 200,
   },
 });

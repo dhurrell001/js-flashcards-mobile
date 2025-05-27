319111ac8a0f56e290d2bb007a9b8faf7bdae86a
David Hurrell <dhurrell@hotmail.co.uk>
2025-05-27 18:56:19 +0100

    feat: Add FlashcardQuizNavButtons component for navigation in flashcard quizzes

---
+++ components/flashcardQuizNavButtons.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import SmallSelectorButton from "./smallSelectorButton";
+import AntDesign from "@expo/vector-icons/AntDesign";
+export default function FlashcardQuizNavButtons({
+  labelArray,
+  handleNext,
+  handlePrev,
+  isQuizsumbitted,
+  isCardReversed,
+  selectedQuizQuestions,
+  currentCardIndex,
+  handleFlip,
+}) {
+  const isLastCard = currentCardIndex === selectedQuizQuestions.length - 1;
+  console.log("currentCardIndex", currentCardIndex);
+  console.log("selectedQuizQuestions.length", selectedQuizQuestions.length);
+  console.log("isLastCard", isLastCard);
+
+  return (
+    <View style={styles.buttonContainer}>
+      <SmallSelectorButton
+        text={labelArray[0]}
+        icon={<AntDesign name="banckward" size={20} color="black" />}
+        onPress={handlePrev}
+      />
+      {isLastCard && (
+        <SmallSelectorButton text={labelArray[1]} onPress={handleFlip} />
+      )}
+      {!isLastCard && (
+        <SmallSelectorButton
+          text={labelArray[2]}
+          icon={<AntDesign name="forward" size={20} color="black" />}
+          onPress={handleNext}
+        />
+      )}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "100%",
+    marginTop: 20,
+  },
+});
